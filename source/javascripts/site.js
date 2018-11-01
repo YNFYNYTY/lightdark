9fcc117fb3d4465aa15580c83820b6d41679be9a
YNFYNYTY <www.rog_drummer@hotmail.com>
2018-11-01 23:44:04 +0100

    First commit

---
+++ source/javascripts/site.js
@@ -0,0 +1,26 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $(".menu-container").fadeOut();
+    } else {
+        $(".menu-container").fadeIn();
+    }
+});
+
+// $(function () {
+//     $('.tlt').textillate({
+//     	loop: true,
+//     	minDisplayTime: 1000,
+//     });
+// });
+$(function(){ 
+$("#memberx").click(function(){
+$(".more-info-x").slideToggle();
+});
+$("#membery").click(function(){
+$(".more-info-y").slideToggle();
+});
+$("#memberz").click(function(){
+$(".more-info-z").slideToggle();
+});
+});
