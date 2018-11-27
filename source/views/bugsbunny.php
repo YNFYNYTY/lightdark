aed361000a0ef2be9ece21b7755f43484e3f8fe4
YNFYNYTY <www.rog_drummer@hotmail.com>
2018-11-27 21:59:29 +0100

    Test 1

--- source/views/bugsbunny.php
+++ source/views/bugsbunny.php
@@ -6,11 +6,11 @@
         $message = trim($_POST["message"]);
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            echo "Ha ocurrido un problema con el envío, por favor inténtelo de nuevo";
             exit;
         }
-        $recipient = "rogerbrizt@gmail.com";
-        $subject = "LIGHT&DARK STUDIO $name";
+        $recipient = "studiolightdark@gmail.com";
+        $subject = "Email of $name from L&D Studio Site";
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
         $email_content .= "Message:\n$message\n";
